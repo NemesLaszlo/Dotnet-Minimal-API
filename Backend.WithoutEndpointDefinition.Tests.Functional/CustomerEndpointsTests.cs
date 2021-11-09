88c558019c08a5cf9d848cee1068114bd5830ab3
NemesLaszlo <wow.laszlo@gmail.com>
2021-11-10 00:02:11 +0100

    Xunit test for Minimal API

---
+++ Backend.WithoutEndpointDefinition.Tests.Functional/CustomerEndpointsTests.cs
@@ -0,0 +1,86 @@
+﻿using Backend.WithoutEndpointDefinition.Interfaces;
+using Backend.WithoutEndpointDefinition.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Backend.WithoutEndpointDefinition.Tests.Functional;
+
+public class CustomerEndpointsTests
+{
+    private readonly ICustomerRepository _customerRepository = Substitute.For<ICustomerRepository>();
+
+    [Fact]
+    public async Task GetCustomerById_ReturnCustomer_WhenCustomerExists()
+    {
+        //Arrange
+        var id = Guid.NewGuid();
+        var customer = new Customer(id, "Leslie Nemes");
+        _customerRepository.GetById(Arg.Is(id)).Returns(customer);
+
+        using var app = new CustomerEndpointsTestsApp(x =>
+        {
+            x.AddSingleton(_customerRepository);
+        });
+
+        var httpClient = app.CreateClient();
+
+        //Act
+        var response = await httpClient.GetAsync($"/customers/{id}");
+        var responseText = await response.Content.ReadAsStringAsync();
+        var customerResult = JsonSerializer.Deserialize<Customer>(responseText);
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        customerResult.Should().BeEquivalentTo(customer);
+    }
+
+    [Fact]
+    public async Task GetCustomerById_ReturnNotFound_WhenCustomerDoesNotExists()
+    {
+        //Arrange
+        _customerRepository.GetById(Arg.Any<Guid>()).Returns((Customer?)null);
+
+        using var app = new CustomerEndpointsTestsApp(x =>
+        {
+            x.AddSingleton(_customerRepository);
+        });
+
+        var guid = Guid.NewGuid();
+        var httpClient = app.CreateClient();
+
+        //Act
+        var response = await httpClient.GetAsync($"/customers/{guid}");
+
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
+
+internal class CustomerEndpointsTestsApp : WebApplicationFactory<Program>
+{
+    private readonly Action<IServiceCollection> _serviceOverride;
+
+    public CustomerEndpointsTestsApp(Action<IServiceCollection> serviceOverride)
+    {
+        _serviceOverride = serviceOverride;
+    }
+
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureServices(_serviceOverride);
+
+        return base.CreateHost(builder);
+    }
+}
+
---
+++ Backend.WithoutEndpointDefinition.Tests.Unit/CustomerEndpointsTests.cs
@@ -0,0 +1,73 @@
+﻿using Backend.WithoutEndpointDefinition.Endpoints;
+using Backend.WithoutEndpointDefinition.Interfaces;
+using Backend.WithoutEndpointDefinition.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+using System;
+using Xunit;
+
+namespace Backend.WithoutEndpointDefinition.Tests.Unit;
+
+public class CustomerEndpointsTests
+{
+    private readonly ICustomerRepository _customerRepository = Substitute.For<ICustomerRepository>();
+
+    [Fact]
+    public void GetCustomerById_ReturnCustomer_WhenCustomerExists()
+    {
+        //Arrange
+        var id = Guid.NewGuid();
+        var customer = new Customer(id, "Leslie Nemes");
+        _customerRepository.GetById(Arg.Is(id)).Returns(customer);
+
+        //Act
+        var result = CustomerEndpoints.GetCustomerById(_customerRepository, id);
+
+        //Assert
+        result.GetOkObjectResultValue<Customer>().Should().BeEquivalentTo(customer);
+        result.GetOkObjectResultStatusCode().Should().Be(200);
+    }
+
+    [Fact]
+    public void GetCustomerById_ReturnNotFound_WhenCustomerDoesNotExists()
+    {
+        //Arrange
+        _customerRepository.GetById(Arg.Any<Guid>()).Returns((Customer?)null);
+
+        //Act
+        var result = CustomerEndpoints.GetCustomerById(_customerRepository, Guid.NewGuid());
+
+        //Assert
+        result.GetNotFoundResultStatusCode().Should().Be(404);
+    }
+
+}
+
+public static class IResultExtensions
+{
+    public static T? GetOkObjectResultValue<T>(this IResult result)
+    {
+        return (T?)Type.GetType("Microsoft.AspNetCore.Http.Result.OkObjectResult, Microsoft.AspNetCore.Http.Results")?
+            .GetProperty("Value",
+                System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public)?
+            .GetValue(result);
+    }
+
+    public static int? GetOkObjectResultStatusCode(this IResult result)
+    {
+        return (int?)Type.GetType("Microsoft.AspNetCore.Http.Result.OkObjectResult, Microsoft.AspNetCore.Http.Results")?
+            .GetProperty("StatusCode",
+                System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public)?
+            .GetValue(result);
+    }
+
+    public static int? GetNotFoundResultStatusCode(this IResult result)
+    {
+        return (int?)Type.GetType("Microsoft.AspNetCore.Http.Result.NotFoundObjectResult, Microsoft.AspNetCore.Http.Results")?
+            .GetProperty("StatusCode",
+                System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public)?
+            .GetValue(result);
+    }
+}
+
